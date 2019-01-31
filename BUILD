17963ab4403a46436ce1fd34ca62fba6a8508718
Jyothrilinga Rao Kurapati <jyothri@gmail.com>
2019-01-31 12:01:27 -0800

    added bazel
    Added bazel WORKSPACE and BUILD files. Also create a trivial source file
    since bazel complains without it.
    
    Testing Done: bazel build //:Project executes without issues
    bazel-bin/Project executes without issues

---
+++ BUILD
@@ -0,0 +1,5 @@
+java_binary(
+    name = "Project",
+    srcs = glob(["src/main/java/edu/jyo/app/*.java"]),
+    main_class = "edu.jyo.app.Application",
+)
---
+++ src/main/java/edu/jyo/app/Application.java
@@ -0,0 +1,8 @@
+package edu.jyo.app;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Started Program");
+        System.out.println("Completed Execution");
+    }
+}
